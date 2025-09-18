79799753ebb87b276930598029255d31af15bc5c
malyok99 <limeon312@gmail.com>
2025-09-18 10:23:39 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,55 @@
+#include <iostream>
+#include <vector>
+
 // file object
+class FileObjectSystem {
+public:
+  virtual void displayInfo() = 0;
+  virtual void serialize(std::ostream) = 0;
+  virtual void deserialize(std::istream) = 0;
+};
 
-
-// file 
-
+// file
+class File:FileObjectSystem {
+public:
+  std::string name;
+  std::vector<char> data;
+  void displayInfo() override {};
+  void serialize(std::ostream) override {};
+  void deserialize(std::istream) override {};
+};
 
 // directory
-
-
-#include <iostream>
+class Directory:FileObjectSystem {
+public:
+  std::string name;
+  std::vector<File> data;
+  void displayInfo() override {
+    std::cout << "Directory name: " << name << std::endl;
+    for(auto &f : data){
+      std::cout << "File name: " << f.name << std::endl;
+    }
+  }
+  void serialize(std::ostream) override {};
+  void deserialize(std::istream) override {};
+};
 
 int main(){
+  // make directory
+  Directory homeDir;
+  homeDir.name = "home";
 
-// make directory
-// add child (file)
-// display info
+  // add child (file)
+  File myFile;
+  myFile.name = "file.txt";
+  homeDir.data.push_back(myFile);
+
+  File myFile1;
+  myFile1.name = "file1.txt";
+  homeDir.data.push_back(myFile1);
+
+  // display info
+  homeDir.displayInfo();
 
   return 0;
 }
