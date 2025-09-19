3c9ef4c44f71df1d22324e96c90ac399822cf769
malyok99 <limeon312@gmail.com>
2025-09-18 20:00:50 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,38 +1,60 @@
 #include <iostream>
 #include <vector>
+#include <memory>
+#include <ctime>
 
-// file object
-class FileObjectSystem {
+// instructor
+class IFileObject {
 public:
-  virtual void displayInfo() = 0;
-  virtual void serialize(std::ostream) = 0;
-  virtual void deserialize(std::istream) = 0;
+  virtual void displayInfo(int tab = 0) = 0;
+  virtual void serialize(std::ostream &os) = 0;
+  virtual void deserialize(std::istream &is) = 0;
 };
 
 // file
-class File:FileObjectSystem {
+class File : public IFileObject {
+private:
+  time_t currentTime = time(0);
+  tm* date = localtime(&currentTime);
 public:
   std::string name;
   std::vector<char> data;
-  void displayInfo() override {};
-  void serialize(std::ostream) override {};
-  void deserialize(std::istream) override {};
+
+  void displayInfo(int tab = 0) override {
+    char buffer[80];
+    strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", date);
+    
+    std::cout << std::string(tab, ' ') << "└─ (" << buffer << ") / " << name << std::endl;
+  }
+  void serialize(std::ostream &os) override {};
+  void deserialize(std::istream &is) override {};
 };
 
 // directory
-class Directory:FileObjectSystem {
+class Directory : public IFileObject {
 public:
   std::string name;
-  std::vector<File> data;
-  void displayInfo() override {
-    std::cout << "Directory name: " << name << std::endl;
-    for(auto &f : data){
-      std::cout << "File name: " << f.name << std::endl;
+  std::vector<std::unique_ptr<IFileObject>> children;
+
+  void displayInfo(int tab = 0) override {
+    std::cout << std::string(tab, ' ') << name << std::endl;
+    for(auto &f : children){
+      f->displayInfo(tab + 2);
     }
   }
-  void serialize(std::ostream) override {};
-  void deserialize(std::istream) override {};
+  void serialize(std::ostream &os) override {};
+  void deserialize(std::istream &is) override {};
+  void addChild(std::unique_ptr<IFileObject> child){
+    children.push_back(std::move(child));
+  }
 };
+
+void addFile(const std::string &name){
+
+}
+void addDirectory(const std::string &name){
+
+}
 
 int main(){
   // make directory
@@ -40,13 +62,18 @@ int main(){
   homeDir.name = "home";
 
   // add child (file)
-  File myFile;
-  myFile.name = "file.txt";
-  homeDir.data.push_back(myFile);
+  auto file1 = std::make_unique<File>();
+  file1->name = "test.txt";
+  homeDir.addChild(std::move(file1));
 
-  File myFile1;
-  myFile1.name = "file1.txt";
-  homeDir.data.push_back(myFile1);
+  auto dir1 = std::make_unique<Directory>();
+  dir1->name = "another_dir";
+
+  auto file2 = std::make_unique<File>();
+  file2->name = "test2.txt";
+  dir1->addChild(std::move(file2));
+
+  homeDir.addChild(std::move(dir1));
 
   // display info
   homeDir.displayInfo();
