c2d9f43e377857babdcbe63d24e361cc61f3bcac
malyok99 <limeon312@gmail.com>
2025-09-19 10:49:01 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <memory>
 #include <ctime>
@@ -24,10 +25,28 @@ public:
     char buffer[80];
     strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", date);
     
-    std::cout << std::string(tab, ' ') << "└─ (" << buffer << ") / " << name << std::endl;
+    std::cout << std::string(tab, ' ') << "\\_ (" << buffer << ") | " << name << std::endl;
   }
-  void serialize(std::ostream &os) override {};
-  void deserialize(std::istream &is) override {};
+  void serialize(std::ostream &os) override {
+    uint32_t nameLen = name.size();
+    os.write(reinterpret_cast<char*>(&nameLen), sizeof(nameLen));
+    os.write(name.data(), nameLen);
+
+    uint32_t dataLen = data.size();
+    os.write(reinterpret_cast<char*>(&dataLen), sizeof(dataLen));
+    os.write(data.data(), dataLen);
+  }
+  void deserialize(std::istream &is) override {
+    uint32_t nameLen = name.size();
+    is.read(reinterpret_cast<char*>(&nameLen), sizeof(nameLen));
+    name.resize(nameLen);
+    is.read(name.data(), nameLen);
+
+    uint32_t dataLen = data.size();
+    is.read(reinterpret_cast<char*>(&dataLen), sizeof(dataLen));
+    data.resize(dataLen);
+    is.read(data.data(), dataLen);
+  }
 };
 
 // directory
@@ -42,18 +61,45 @@ public:
       f->displayInfo(tab + 2);
     }
   }
-  void serialize(std::ostream &os) override {};
-  void deserialize(std::istream &is) override {};
+  void serialize(std::ostream &os) override {
+    uint32_t nameLen = name.size();
+    os.write(reinterpret_cast<char*>(&nameLen), sizeof(nameLen));
+    os.write(name.data(), nameLen);
+
+    uint32_t dataLen = children.size();
+    os.write(reinterpret_cast<const char*>(&dataLen), sizeof(dataLen));
+    for (const auto& f : children) {
+      f->serialize(os);
+    }
+  }
+  void deserialize(std::istream &is) override {
+    uint32_t nameLen;
+    is.read(reinterpret_cast<char*>(&nameLen), sizeof(nameLen));
+    name.resize(nameLen);
+    is.read(name.data(), nameLen);
+
+    uint32_t dataLen;
+    is.read(reinterpret_cast<char*>(&dataLen), sizeof(dataLen));
+    children.resize(dataLen);
+    for (auto& f : children) {
+      f->deserialize(is);
+    }
+  }
   void addChild(std::unique_ptr<IFileObject> child){
     children.push_back(std::move(child));
   }
 };
 
-void addFile(const std::string &name){
-
+auto addFile(const std::string &name){
+  auto file = std::make_unique<File>();
+  file->name = name;
+  return file;
 }
-void addDirectory(const std::string &name){
 
+auto addDirectory(const std::string &name){
+  auto dir = std::make_unique<Directory>();
+  dir->name = "another_dir";
+  return dir;
 }
 
 int main(){
@@ -62,15 +108,12 @@ int main(){
   homeDir.name = "home";
 
   // add child (file)
-  auto file1 = std::make_unique<File>();
-  file1->name = "test.txt";
+  auto file1 = addFile("empty.txt");
   homeDir.addChild(std::move(file1));
 
-  auto dir1 = std::make_unique<Directory>();
-  dir1->name = "another_dir";
+  auto dir1 = addDirectory("dir1");
 
-  auto file2 = std::make_unique<File>();
-  file2->name = "test2.txt";
+  auto file2 = addFile("secret.txt");
   dir1->addChild(std::move(file2));
 
   homeDir.addChild(std::move(dir1));
